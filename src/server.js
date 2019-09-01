284f2fe737441ea3c0d93348b42f99c563fed951
durasjan <durasjan1@gmail.com>
2019-09-01 13:51:46 +0200

    Adds some files

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const server = express();
+const port = 3000;
+const corsOptions = {
+    origin: 'http://10.0.0.13:1234',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+
+server.get('/api/:relay', cors(corsOptions), (req, res) => res.send(req.params.relay));
+server.post('/api/:relay', cors(corsOptions), (req, res) => res.send(req.params.relay));
+
+server.listen(port, () => console.log(`Server listening on port ${port}`));
